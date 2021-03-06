0f0aa856c8a22e9a32207dd4113965888f1667b4
MadsL59 <78481897+MadsL59@users.noreply.github.com>
2021-03-06 21:56:01 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,14 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-64.45px";
+  }
+  prevScrollpos = currentScrollPos;
+}
